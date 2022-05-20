46673e6198c657759f9737adaef29293d7879cc1
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2022-05-21 01:25:08 +0900

    Add files via upload

---
+++ FN.js
@@ -0,0 +1,21 @@
+const scriptName = "FN";
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
