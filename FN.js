5309ed0ef7c4afb99bd63cac0ea6a4c8fccb6d41
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2022-05-21 12:30:54 +0900

    Update FN.js

--- FN.js
+++ FN.js
@@ -1,4 +1,5 @@
 const scriptName = "FN";
+var allsee = new Array(1000).join(String.fromCharCode(847));
 /**
  * (string) room
  * (string) sender
@@ -9,7 +10,32 @@ const scriptName = "FN";
  * (string) packageName
  */
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  
+  try {
+      var Flist = DataBase.getDataBase('욕감지리스트').split('\n');
+  }catch(e) {
+      var Flist = [''];
+  }
+  try {
+      var Adminlist = DataBase.getDataBase('Adminlist').split('\n');
+  }catch(e) {
+      var Adminlist = [''];
+  }
+  var message = msg.split(' ');
+  if (message[0] == ';금지단어등록') {
+      if (Adminlist.includes(sender)) {
+        let cutting = msg.replace(';금지단어등록 ', '');
+        DataBase.setDataBase('욕감지리스트', DataBase.getDataBase('욕감지리스트')+'\n'+cutting);
+        replier.reply('금지단어 리스트에 '+cutting+'을(를) 추가했습니다.');
+    } else {
+        replier.reply('관리자 권한을 봇 제작자에게 추가해달라고 요청하십시오.');
+    }
+  }
+  if (msg.includes(Flist)) {
+      rudrh = DataBase.getDataBase(sender+'의 경고 횟수');
+      DataBase.setDataBase(sender+'의 경고 횟수', rudrh+1);
+      rudrh = DataBase.getDataBase(sender+'의 경고 횟수');
+      replier.reply('[ 알림 ]'+allsee+'\n'+sender+'이가 규칙에 어긋나는 행동을 하였습니다.\n경고 횟수 : '+rudrh);
+  }
 }
 
 function onStart(activity) {}
